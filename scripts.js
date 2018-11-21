acaab23ea96ddfb2b9c85380eac17025ddae448b
Corina Pelichowski <corina@danitt.com>
2018-11-21 14:09:58 +1000

    addition of skip ahead and skip back in the video

--- scripts.js
+++ scripts.js
@@ -12,16 +12,18 @@ function togglePlay() {
   const method = video.paused ? 'play' : 'pause';
   video [method]();
 }
-
 function updateButton() {
   const icon = this.paused ? '▶️' : '⏸';
   toggle.textContent = icon;
 }
-
-
+function skip () {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
 
 /*Hook up the Event Listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
